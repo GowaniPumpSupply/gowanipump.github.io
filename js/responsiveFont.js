7d2c87184cd922722b5743e5d725e2fd2ca1090a
Ata Gowani <atagowani@gmail.com>
2017-06-01 10:58:03 -0500

    move flowtype

---
+++ js/responsiveFont.js
@@ -0,0 +1,15 @@
+$('.footer').flowtype({
+  minFont   : 8,
+  maxFont   : 15,
+  fontRatio : 40
+}),
+$('.cover').flowtype({
+  minFont   : 20,
+  maxFont   : 60,
+  fontRatio : 40
+})
+$('.navbar-brand').flowtype({
+  minFont   : 20,
+  maxFont   : 60,
+  fontRatio : 40
+});
