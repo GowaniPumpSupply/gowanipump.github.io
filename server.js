f23013978906897f6ed107a885663472f38246ed
Ata Gowani <atagowani@gmail.com>
2017-06-01 23:52:37 -0500

    add server

---
+++ server.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var staticPath = path.join(__dirname, '/');
+app.use(express.static(staticPath));
+
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+  console.log('Ctrl + C to stop');
+});
