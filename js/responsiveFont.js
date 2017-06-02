ed23fd43cafbd16d1f19c6b9c2b7280f7d89c963
Ata Gowani <atagowani@gmail.com>
2017-06-01 19:34:24 -0500

    organize code

--- js/responsiveFont.js
+++ js/responsiveFont.js
@@ -7,8 +7,13 @@ $('.cover').flowtype({
   minFont   : 20,
   maxFont   : 60,
   fontRatio : 40
-})
-$('.navbar-brand').flowtype({
+}),
+$('#about').flowtype({
+  minFont   : 20,
+  maxFont   : 60,
+  fontRatio : 40
+}),
+$('.navbar').flowtype({
   minFont   : 20,
   maxFont   : 60,
   fontRatio : 40
