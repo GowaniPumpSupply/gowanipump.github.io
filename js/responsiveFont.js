bae5f3869ce19910a3547a2d7848d48564bc1d98
Ata Gowani <atagowani@gmail.com>
2017-06-08 19:24:46 -0500

    beautify contact page

--- js/responsiveFont.js
+++ js/responsiveFont.js
@@ -17,4 +17,19 @@ $('#who_are_we').flowtype({
   minFont   : 30,
   maxFont   : 90,
   fontRatio : 10
+}),
+$('#description').flowtype({
+  minFont   : 20,
+  maxFont   : 35,
+  fontRatio : 25
+}),
+$('.contact-title').flowtype({
+  minFont   : 30,
+  maxFont   : 85,
+  fontRatio : 10
+}),
+$('.contact-sub-title').flowtype({
+  minFont   : 15,
+  maxFont   : 35,
+  fontRatio : 20
 });
